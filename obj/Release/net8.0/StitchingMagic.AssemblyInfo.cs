42bed1e3364fd639f33f77eea1b801d1f841cab3
y-shrm <yash.sharma7@cognizant.com>
2024-06-05 17:01:11 +0530

    commit

--- obj/Release/net8.0/StitchingMagic.AssemblyInfo.cs
+++ obj/Release/net8.0/StitchingMagic.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("StitchingMagic")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee308917fe0d3458f4cc3d21cdab2b0cd5d62d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StitchingMagic")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,20 +3,53 @@
 
 // Write your JavaScript code.
 
-//document.addEventListener('DOMContentLoaded', (event) => {
-//    // Wait for the DOM to be loaded
-//    document.querySelector('.navbar-toggler').addEventListener('click', function () {
-//        // Add click event listener to the toggler button
-//        let navbarMenu = document.querySelector('.navbar-collapse');
-//        // Select the navbar menu
-//        if (navbarMenu.classList.contains('collapse')) {
-//            // Check if the menu is collapsed
-//            navbarMenu.classList.remove('collapse');
-//            // If it is, remove the 'collapse' class to show the menu
-//        } else {
-//            navbarMenu.classList.add('collapse');
-//            // Otherwise, add the 'collapse' class to hide the menu
-//        }
-//    });
-//});
+
+    //// Initially disable the dark theme
+    //document.getElementById('dark').disabled = true;
+
+    //// Listen for changes on the checkbox
+    //document.getElementById('darkModeToggle').addEventListener('change', function() {
+    //    if (this.checked) {
+    //    // If the checkbox is checked, enable the dark theme and disable the light theme
+    //    document.getElementById('dark').disabled = false;
+    //document.getElementById('light').disabled = true;
+    //    } else {
+    //    // If the checkbox is unchecked, enable the light theme and disable the dark theme
+    //    document.getElementById('dark').disabled = true;
+    //document.getElementById('light').disabled = false;
+    //    }
+    //});
+
+
+
+// Check if a theme is saved in localStorage
+if (localStorage.getItem('theme')) {
+    // If a theme is saved, use it
+    if (localStorage.getItem('theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        document.getElementById('darkModeToggle').checked = true;
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+        document.getElementById('darkModeToggle').checked = false;
+    }
+} else {
+    // If no theme is saved, default to light theme
+    document.documentElement.setAttribute('data-bs-theme', 'light');
+}
+
+// Listen for changes on the checkbox
+document.getElementById('darkModeToggle').addEventListener('change', function () {
+    if (this.checked) {
+        // If the checkbox is checked, set the data-bs-theme attribute to dark
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        localStorage.setItem('theme', 'dark');  // Save the theme in localStorage
+    } else {
+        // If the checkbox is unchecked, set the data-bs-theme attribute to light
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+        localStorage.setItem('theme', 'light');  // Save the theme in localStorage
+    }
+});
+
+
+
 
